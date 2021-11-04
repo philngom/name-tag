f05b2d41731f4afacafd5898081fd1b1872c5b5c
Philippe Ngom <philngom@gmail.com>
2021-11-04 11:16:22 -0700

    change name and change color functionality working. Want to work on creating drop down menu for colors

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+//GRABBING ELEMENTS FROM THE DOM
+const newName = document.querySelector('#input-name');
+const oldName = document.querySelector('.name');
+const changeButton = document.querySelector('#change');
 const tag = document.querySelector('#tag');
 const greenButton = document.querySelector('.green');
 const blueButton = document.querySelector('.blue');
+
+console.log(newName.textContent);
+// console.log(oldName.textContent);
+
+// EVENTS
+changeButton.addEventListener('click', () => {
+  console.log(newName.textContent)
+  oldName.textContent = newName.value;
+})
 
 greenButton.addEventListener('click', () => {
   tag.style.backgroundColor = "green";
